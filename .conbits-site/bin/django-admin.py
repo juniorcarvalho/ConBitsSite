e5e30c7a0c3a3193e00298c05b162f72935c562e
Júnior Carvalho <joseadolfojr@gmail.com>
2016-11-21 14:57:39 -0200

    modificações

---
+++ .conbits-site/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/junior/projetos/conbits-site/.conbits-site/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- conbits/urls.py
+++ conbits/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from conbits.core.views import home
 
 urlpatterns = [
-    url(r'^$', 'conbits.core.views.home'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
